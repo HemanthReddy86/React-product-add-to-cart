093b46dfd96b8732b0c3b81b614ddca3a7311ec6
Heamnth <gopuhemanth1986@gmail.com>
2025-06-07 14:22:39 +0530

    Product add to cart

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Nav from './Components/Nav/Nav'
+import Index from './Components/Pages'
+
+function App() {
+  
+
+  return (
+    <>
+      <Nav />
+      <Index />
+    </>
+  )
+}
+
+export default App
